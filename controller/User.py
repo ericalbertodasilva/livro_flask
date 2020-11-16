32d2ad2145cd7b436c22ef57f490de4f7531ee82
Eric A Silva <ercker@gmail.com>
2020-11-16 16:20:07 -0300

    chapter 10: auth JWT

--- controller/User.py
+++ controller/User.py
@@ -1,8 +1,45 @@
 from model.User import User
+
+from datetime import datetime, timedelta
+import hashlib, base64, json, jwt
+from config import app_config, app_active
+config = app_config[app_active]
+
 class UserController():
     def __init__(self):
         self.user_model = User()
     
+    def verify_auth_token(self, access_token):
+        status = 401
+        try:
+            jwt.decode(access_token, config.SECRET, algorithm='HS256')
+            message = 'Token válido'
+            status = 200
+        except jwt.ExpiredSignatureError:
+            message = 'Token expirado, realize um novo login'
+        except:
+            message = 'Token inválido'
+        return {
+            'message': message,
+            'status': status
+        }
+    
+    def generate_auth_token(self, data, exp=30, time_exp=False):
+        if time_exp == True:
+            date_time = data['exp']
+        else:
+            date_time = datetime.utcnow() + timedelta(minutes=exp)
+        
+        dict_jwt = {
+            'id': data['id'],
+            'username': data['username'],
+            "exp": date_time
+        }
+
+        access_token = jwt.encode(dict_jwt, config.SECRET, algorithm='HS256')
+        
+        return access_token
+
     def login(self, email, password):
         """
             Pega os dados de e-mail e salva no
